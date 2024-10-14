d975c5bb666c63e374852de5d137b74a020e12a2
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2024-10-14 14:33:24 +0300

    ADD: SVG Webpack Next config, some env example

--- goodname/next.config.mjs
+++ goodname/next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production'
+
+const remotePatterns = [
+  {
+    hostname: 'localhost',
+    port: ''
+  }
+]
+
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns
+  },
+  assetPrefix: isProd ? process.env.PUBLIC_URL : undefined
+}
 
 export default nextConfig;
